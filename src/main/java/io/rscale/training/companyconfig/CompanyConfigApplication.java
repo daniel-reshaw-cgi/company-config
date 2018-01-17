7a544e7b77a825a262605c06e31459688005135e
daniel-reshaw-cgi <daniel.reshaw.cgi@gmail.com>
2018-01-17 13:43:02 -0700

    Initial commit

---
+++ src/main/java/io/rscale/training/companyconfig/CompanyConfigApplication.java
@@ -0,0 +1,12 @@
+package io.rscale.training.companyconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyConfigApplication.class, args);
+	}
+}
